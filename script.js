c681d8779d393bded0996e4e38965a1c5653bee9
CodeCara <149534506+CodeCara@users.noreply.github.com>
2024-06-30 11:31:00 +0200

    Final version

--- script.js
+++ script.js
@@ -1,84 +1,25 @@
-//let computerChoice;
-//function getComputerChoice() {
-//let randomNumber = Math.floor((Math.random()*3));
 
-
-//other commented out code is correct, but trying in diff place
-//what to do if 0 is generated?
-
-
-
-//switch (randomNumber) {
-//case 0: computerChoice = 'Rock';
-//break;
-//case 1: computerChoice = 'Paper';
-//break;
-//case 2: computerChoice = 'Scissors';
-//break;
-
-
-//};
-
-
-//computerChoice = computerChoice.toUpperCase();
-//console.log(computerChoice);
-//return computerChoice;
-//}
-
-//let humanChoice;
-
-//computerChoice = getComputerChoice();
-
-
-//function getHumanChoice() {
-  //  humanChoice =  prompt ('Enter Rock, Paper or Scissors');
-    //Better way of string comparison?
-   // humanChoice = humanChoice.toUpperCase();
-    //console.log(humanChoice);
-  
-//}
-
-//getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 
 
 function playGame() {
-for (let i=0; i<5; i++){
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors');
+let runningScore = document.querySelector('.running-score');
+let outputMessage = document.querySelector('.output-message');
 
-  let humanChoice;
-
-
-
-
-function getHumanChoice() {
-    humanChoice =  prompt ('Enter Rock, Paper or Scissors');
-    //Better way of string comparison?
-    humanChoice = humanChoice.toUpperCase();
-    //console.log(humanChoice);
   
-}
-
-getHumanChoice();
-
-
-
-
-
-
-
-
-
+  let humanChoice;
 
 
   let computerChoice;
   function getComputerChoice() {
   let randomNumber = Math.floor((Math.random()*3));
-  //what to do if 0 is generated?
   
-  
-  
+
   switch (randomNumber) {
   case 0: computerChoice = 'Rock';
   break;
@@ -87,87 +28,93 @@ getHumanChoice();
   case 2: computerChoice = 'Scissors';
   break;
   
-  
   };
   
   
   computerChoice = computerChoice.toUpperCase();
-  //console.log(computerChoice);
+  //cl to see how working
+ console.log(computerChoice);
+ 
   return computerChoice;
   }
 
-  computerChoice = getComputerChoice();
-
-
-  //let humanChoice;
-
-
-
-
-//function getHumanChoice() {
- //   humanChoice =  prompt ('Enter Rock, Paper or Scissors');
-    //Better way of string comparison?
-   // humanChoice = humanChoice.toUpperCase();
-    //console.log(humanChoice);
   
-//}
+  rock.addEventListener("click", function(e) {humanChoice=e.target.value;
+    getComputerChoice();
+    console.log(humanChoice);
+    playRound();
+  
+  });
 
-//getHumanChoice();
 
-
-//let humanScore = 0;
-//let computerScore = 0;
-
-
-function playRound() {
-  //let humanScore = 0;
-//let computerScore = 0;
+  paper.addEventListener("click", function(e){humanChoice=e.target.value;
+    getComputerChoice();
+    console.log(humanChoice);
+    playRound();
+  });
  
+
+
+  scissors.addEventListener("click", function(e){humanChoice=e.target.value;
+    getComputerChoice();
+    console.log(humanChoice);
+    playRound();
+  });
+
+  
+function playRound() {
+
    if (humanChoice == 'ROCK' && computerChoice == 'PAPER') {
     computerScore ++;
-   // console.log(computerScore);
-    console.log('You lose - paper beats rock!');
+   
+    outputMessage.innerText ="You lose - paper beats rock!";
+   
    }
    else if (humanChoice == 'ROCK' && computerChoice == 'SCISSORS')
     {humanScore ++;
-console.log('You win - rock beats scissors!'); }
-//put generic cond at end if the two are equal for a draw
+outputMessage.innerText ="You win - rock beats scissors!";
+
+ }
+
 else if (humanChoice == 'PAPER' && computerChoice == 'ROCK') {
   computerScore ++;
- // console.log(computerScore);
-  console.log('You win - paper beats rock!');
+ 
+ outputMessage.innerText ="You win - paper beats rock!";
   humanScore ++;
+ 
  }
  else if (humanChoice == 'PAPER' && computerChoice == 'SCISSORS')
   {
-console.log('You lose - scissors beats paper!'); 
-computerScore++;}
+    outputMessage.innerText ="You lose - scissors beats paper!"; 
+computerScore++;
+
+}
 else if (humanChoice == 'SCISSORS' && computerChoice == 'PAPER')
   {
-console.log('You win - scissors beats paper!'); 
-humanScore++;}
+    outputMessage.innerText ="You win - scissors beats paper!"; 
+humanScore++;
+
+}
 else if (humanChoice == 'SCISSORS' && computerChoice == 'ROCK')
   {
-console.log('You lose - rock beats scissors!'); 
-computerScore++;}
-else if (humanChoice == computerChoice)
-  {console.log('You both picked the same! Its a draw!');
+    outputMessage.innerText ="You lose - rock beats scissors!"; 
+computerScore++;
 
+}
+else if (humanChoice == computerChoice)
+  {outputMessage.innerText ="You both picked the same! Its a draw!";
+console.log(computerScore);
+console.log(humanScore);
   
 }
-
+runningScore.innerText = `Running scores: Your score = ${humanScore}, Computer score = ${computerScore}`;
+if (computerScore == 5) {runningScore.innerText = `Running scores: Your score = ${humanScore}, Computer score = ${computerScore}.  The computer wins!`;}
+else if (humanScore == 5) {runningScore.innerText = `Running scores: Your score = ${humanScore}, Computer score = ${computerScore}.  You win!`;}
 }
-playRound();
-
-
 }
 
-if (humanScore > computerScore) {
 
-  console.log('Congratulations, you beat the computer Odinite!');}
 
-  else  {console.log('Sorry, the computer won this time.  Better luck next time Odinite!');}
-
-}
-//playGame();
 playGame();
+
+
